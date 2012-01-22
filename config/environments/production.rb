0287c2acd9697df66d7b09f0313d2269b38d42b7
Peter Keen <peter.keen@bugsplat.info>
2012-01-21 18:56:58 -0800

    Configure ActionMailer to use mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,14 @@ Ideas::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'], 
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'bugsplat-notes.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
