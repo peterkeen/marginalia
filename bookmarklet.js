31e18d52c7da6245d11aee7d1e85cde0b221cd7b
Peter Keen <peter.keen@bugsplat.info>
2012-01-24 07:56:21 -0800

    Raw bookmarklet. process with make_bookmarklet.pl

---
+++ bookmarklet.js
@@ -0,0 +1,6 @@
+(function () {
+    var t = document.title;
+    var l = window.location;
+    var f = 'peter.keen@bugsplat.info';
+    window.open("https://bugsplat-notes.herokuapp.com/notes/new?note[title]="+t+"&note[body]=%23bookmark%0A%0A"+l+"&note[from_address]="+f);
+})();
