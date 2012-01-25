97ec16a783ded0765564dad285cdbabfd981b319
Peter Keen <peter.keen@bugsplat.info>
2012-01-24 19:20:15 -0800

    I'm not in central time what the fuck

--- lib/render_with_tags.rb
+++ lib/render_with_tags.rb
@@ -8,7 +8,7 @@ class RenderWithTags < Redcarpet::Render::HTML
 
     current_date = nil
     full_document.gsub!(/@(\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d)/) do |match|
-      date = DateTime.parse($1).to_time.in_time_zone('Central Time (US & Canada)')
+      date = DateTime.parse($1).to_time.in_time_zone('Pacific Time (US & Canada)')
       "* * *\n**#{date.strftime('%Y %b %d %l:%M %P')}**\n\n" 
     end
 
