30c93693cf1452e119f829ecc4f7cdc0e89fdf5f
Peter Keen <peter.keen@bugsplat.info>
2012-01-24 19:00:54 -0800

    Format timestamps usefully

--- lib/render_with_tags.rb
+++ lib/render_with_tags.rb
@@ -1,7 +1,17 @@
+require 'date'
+
 class RenderWithTags < Redcarpet::Render::HTML
   def preprocess(full_document)
-    full_document.gsub(/#(\w+)/) do |match|
+    full_document.gsub!(/#(\w+)/) do |match|
       "[##{$1}](/tags/#{$1})"
     end
+
+    current_date = nil
+    full_document.gsub!(/@(\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d)/) do |match|
+      date = Date.parse($1).to_time.in_time_zone('Central Time (US & Canada)')
+      "* * *\n**#{date.strftime('%Y %b %d %l:%M %P')}**\n\n" 
+    end
+
+    full_document
   end
 end
