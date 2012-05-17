59adf6db20180f2a3042d38db71e7c8562ed5605
Peter Keen <peter.keen@bugsplat.info>
2012-05-16 18:03:11 -0700

    Reinstate blah

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,4 +4,4 @@
     var f = 'peter.keen@bugsplat.info';
     window.open("https://bugsplat-notes.herokuapp.com/notes/new?note[title]="+t+"&note[body]=%23bookmark%0A%0A"+l+"&note[from_address]="+f);
 })();
-
+//blah
