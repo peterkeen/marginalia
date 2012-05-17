a38a51599b4a6a2e1b47b06d3bdc24c495ef319d
Peter Keen <peter.keen@bugsplat.info>
2012-05-17 14:10:39 -0700

    change some hardcoded urls to different hardcoded paths

--- bookmarklet.js
+++ bookmarklet.js
@@ -2,6 +2,6 @@
     var t = document.title;
     var l = window.location;
     var f = 'peter.keen@bugsplat.info';
-    window.open("https://bugsplat-notes.herokuapp.com/notes/new?note[title]="+t+"&note[body]=%23bookmark%0A%0A"+l+"&note[from_address]="+f);
+    window.open("https://notes.bugsplat.info/notes/new?note[title]="+t+"&note[body]=%23bookmark%0A%0A"+l+"&note[from_address]="+f);
 })();
 //blah
