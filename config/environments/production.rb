7eb02ea9f966393ab380b10cdd6857a63b0d14f8
Pete Keen <peter.keen@bugsplat.info>
2012-08-31 16:28:51 -0700

    switch middleware order

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Ideas::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.middleware.use Rack::SslEnforcer, :hsts => true
   config.middleware.use Rack::WWW
+  config.middleware.use Rack::SslEnforcer, :hsts => true
   
   # Code is not reloaded between requests
   config.cache_classes = true
