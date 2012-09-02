7a0180766ccf81bcad97d8bd84bd8dc80fe07728
Pete Keen <peter.keen@bugsplat.info>
2012-09-01 20:30:26 -0700

    Install google analytics; clean up read_only layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,5 +34,20 @@ module ApplicationHelper
     end
   end
 
+  def google_analytics_tag
+    return '' unless Rails.env.production?
+    javascript_tag """
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5663087-8']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+"""
+  end
 
 end
