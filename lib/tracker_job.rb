756996839dd0a1dcc8482aafcadd95daf97470b3
Pete Keen <peter.keen@bugsplat.info>
2012-09-02 20:17:45 -0700

    Fix the tracker job

--- lib/tracker_job.rb
+++ lib/tracker_job.rb
@@ -1,6 +1,7 @@
 class TrackerJob < Struct.new(:event, :request_env, :properties)
   def perform
-    mp = Mixpanel::Tracker.new(ENV['MIXPANEL_TOKEN', request_env)
+    return unless ENV['MIXPANEL_TOKEN']
+    mp = Mixpanel::Tracker.new(ENV['MIXPANEL_TOKEN'], request_env)
     mp.track_event(event, properties)
   end
 end
