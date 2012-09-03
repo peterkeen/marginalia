b44c07e4bc62aa9e34e171a03060e5f66613ee89
Pete Keen <peter.keen@bugsplat.info>
2012-09-02 21:13:10 -0700

    augh

--- lib/tracker_job.rb
+++ lib/tracker_job.rb
@@ -1,5 +1,9 @@
 class TrackerJob < Struct.new(:event, :request_env, :properties, :token)
   def perform
+    p event
+    p request_env
+    p properties
+    p token
     return unless token
     mp = Mixpanel::Tracker.new(token, request_env)
     mp.track_event(event, properties)
