ddc887d76feba4e88c9e558e0491dfc3b7263ed8
Pete Keen <peter.keen@bugsplat.info>
2012-09-05 19:10:12 -0700

    Revert "Add rails_admin"
    This reverts commit ab26bc0d827dc072afc4bbfda9837edad6ff8e41.

--- Gemfile
+++ Gemfile
@@ -27,7 +27,6 @@ gem 'mixpanel'
 gem 'memcachier'
 gem 'dalli'
 gem 'aws-s3'
-gem 'rails_admin'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120906013224) do
+ActiveRecord::Schema.define(:version => 20120905010415) do
 
   create_table "alternatives", :force => true do |t|
     t.integer "experiment_id"
@@ -70,19 +70,6 @@ ActiveRecord::Schema.define(:version => 20120906013224) do
   end
 
   add_index "notes", ["slug"], :name => "index_notes_on_slug"
-
-  create_table "rails_admin_histories", :force => true do |t|
-    t.text     "message"
-    t.string   "username"
-    t.integer  "item"
-    t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
