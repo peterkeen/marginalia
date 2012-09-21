b756dbab848dab43994e2011772001109b522de9
Pete Keen <peter.keen@bugsplat.info>
2012-09-20 20:51:35 -0700

    Remove cloudfront. it causes nothing but problems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Ideas::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "https://d1wxb68s7urjp9.cloudfront.net"
+  #config.action_controller.asset_host = "https://d1wxb68s7urjp9.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
