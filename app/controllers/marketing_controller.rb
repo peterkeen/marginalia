6ccf5447467a3672e191f96b3e7f310f29ac124b
Pete Keen <peter.keen@bugsplat.info>
2012-09-23 08:22:44 -0700

    switch back to index page

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -31,7 +31,7 @@ class MarketingController < ApplicationController
   def index
     respond_to do |format|
       format.html do
-        render 'online-writing-application', :layout => 'microsite'
+        render :index
       end
     end
   end
